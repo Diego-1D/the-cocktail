1c6aa4622c7f9578c9775b7e4fff5811aae3e483
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2022-01-30 20:30:04 -0300

    Inserção do AppProvider

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
 import App from './App';
+import { AppProvider } from './services/context';
 
 ReactDOM.render(
-    <App />,
+  <AppProvider>
+    <App />
+  </AppProvider>,
   document.getElementById('root')
 );
