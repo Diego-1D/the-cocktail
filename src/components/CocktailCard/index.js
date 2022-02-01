e71c6796cf946f9489aaa6a679034c61e5cec021
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2022-01-31 23:28:20 -0300

    Link

--- src/components/CocktailCard/index.js
+++ src/components/CocktailCard/index.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import './styles.css';
 import { Card } from 'antd';
+import { Link } from 'react-router-dom';
 
 const CocktailCard = ({ id, name, image, info, glass }) => {
     return (
         <>
-            <Card
-                key={id}
-                className='card'
-                hoverable
-                cover={<img className='card_img' src={image} />}
-            >
-                <p className='title'>{name}</p>
-                <p className='subtitle'>{glass}</p>
-                <p className='title_gray'>{info}</p>
-            </Card>
+            <Link to={`/cocktaildetails/${id}`}>
+                <Card
+                    key={id}
+                    className='card'
+                    hoverable
+                    cover={<img className='card_img' src={image} />}
+                >
+                    <p className='title'>{name}</p>
+                    <p className='subtitle'>{glass}</p>
+                    <p className='title_gray'>{info}</p>
+                </Card>
+            </Link>
         </>
     );
 };
