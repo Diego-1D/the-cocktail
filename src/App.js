1fa11a044ec04c8bea5e7f93e4e80d442a1cfec6
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2022-01-31 23:26:35 -0300

    Configurações das rotas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
+import Navbar from './components/Navbar';
+import CocktailDetails from './pages/CocktailDetails';
 import Home from './pages/Home';
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <BrowserRouter>
+      <div className='container_app'>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path='/cocktaildetails/:id' element={<CocktailDetails />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
